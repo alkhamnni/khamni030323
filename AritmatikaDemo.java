f5958679d445ff97b221819493f3382c016f9d61
alkhamnni <126839924+alkhamnni@users.noreply.github.com>
2023-03-06 13:32:38 +0700

    Add files via upload

---
+++ AritmatikaDemo.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alkhamni030323;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class AritmatikaDemo 
+{
+ public static void main(String[] args )
+ {
+     int i = 37;
+     int j = 42;
+     double x = 27.475;
+     double y = 7.22;
+     System.out.println("Variable values...");
+     System.out.println("   i = " + i);
+     System.out.println("   j = " + j);
+     System.out.println("   x = " + x);
+     System.out.println("   y = " + y);
+     
+     
+     //penjumlahan angka
+     System.out.println("adding...");
+     System.out.println("   i + j = " + (i + j));
+     System.out.println("   x + y = " + (x + y));
+     
+     //pengurangan angka
+     System.out.println("subtracing...");
+     System.out.println("   i - j = " + (i - j));
+     System.out.println("   x - y = " + (x - y));
+     
+     //perkalian angka
+     System.out.println("multiplying...");
+     System.out.println("   i * j = " + (i * j));
+     System.out.println("   x * y = " + (x * y));
+     
+     //pembagian angka
+     System.out.println("dividing...");
+     System.out.println("   i / j = " + (i / j));
+     System.out.println("   x / y = " + (x / y));
+     
+     //menghitung hasil modulus dari pembagian
+     System.out.println("computing the remainder...");
+     System.out.println("   i % j = " + (i % j));
+     System.out.println("   x % y = " + (x % y));
+     
+     //type penggbungan
+     System.out.println("mixing types...");
+     System.out.println("   j + y = " + (j + y));
+     System.out.println("   i * X = " + (i * x));
+     
+ }
+}
---
+++ VariabelSample.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alkhamni030323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class VariabelSample 
+{
+ public static void main(String[] args)
+   //deklarasi a tipe data dengan nama variable
+   //result dan tipe data boolean
+ { boolean result;
+ 
+    char option;
+    option = 'C'; //menandai 'C' sebagai option
+    
+    //deklarasi tipe data dengan nama variabel
+    //grade, double tipe data dan telan di inisialisasi
+    //to 0.0
+    double grade = 0.0;
+ }
+}
